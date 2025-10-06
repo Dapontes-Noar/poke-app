f0c0f746439a4c4f65758700defe878143c33b1d
dapontes-noar <dianaa2054@gmail.com>
2025-10-05 19:17:27 -0500

    Refactor bottom navigation widget and update shared providers

--- lib/shared/di/shared_providers.dart
+++ lib/shared/di/shared_providers.dart
@@ -5,4 +5,5 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'shared_providers.g.dart';
 
 @Riverpod(keepAlive: true)
-Widget pokeBottomNavigationBar(Ref ref) => const PokeBottomNavigationBarWidget();
+Widget pokeBottomNavigationBar(Ref ref) =>
+    const PokeBottomNavigationBarWidget();
